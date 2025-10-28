49cd7dc8166209ba17f2eeca8d2babac730b3554
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-10-28 18:44:47 +0530

    update and deleted added

--- src/app.js
+++ src/app.js
@@ -3,15 +3,43 @@ const app = express();
 const port = 3000;
 const db = require("./config/database");
 const User = require("./models/user");
+app.use(express.json());
 
 app.post("/signup", async (req, res) => {
-  const newUser = new User({
-    name: "John Doe",
-    age: 30,
-    email: "tHc3o@example.com",
-  });
+  console.log(req.body);
+  const newUser = new User(req.body);
   await newUser.save().then(() => console.log("User saved"));
   res.status(201).send("User signed up");
+});
+
+app.get("/users", async (req, res) => {
+  console.log("req", req);
+  try {
+    const users = await User.find({ name: "John Doe" });
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.patch("/users", async (req, res) => {
+  console.log("req", req.body);
+  const updatedUser = await User.findByIdAndUpdate(req.body._id, req.body, {
+    new: true,
+  });
+  console.log("updatedUser", updatedUser);
+  res.status(201).send("Updated User", updatedUser);
+});
+
+app.delete("/users/:id", async (req, res) => {
+  console.log("req", req.params.id);
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    console.log("deletedUser", deletedUser);
+    res.status(200).send("Deleted User");
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 db().then(() => {
