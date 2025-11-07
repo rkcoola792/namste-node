bd168c51d276eff6e74788121b07ee44e5a8d538
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-11-07 17:04:20 +0530

    connection accepted and rejected made

--- src/routes/request.js
+++ src/routes/request.js
@@ -2,7 +2,8 @@ const express = require("express");
 const requestRouter = express.Router();
 const ConnectionRequest = require("../models/connectionRequest");
 const userAuth = require("../middleware/auth");
-
+const User = require("../models/user");
+//sending like or ignoring
 requestRouter.post(
   "/request/send/:status/:toUserId",
   userAuth,
@@ -36,6 +37,41 @@ requestRouter.post(
       });
       connection.save();
       res.send("Connection request sent");
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  }
+);
+
+//accepting or rejecting request
+requestRouter.post(
+  "/request/review/:status/:userId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { status, userId } = req.params;
+      const allowedStatus = ["rejected", "accepted"];
+      if (!allowedStatus.includes(status)) {
+        throw new Error("Invalid status");
+      }
+      const userExists = await User.findById(userId);
+      if (!userExists) {
+        throw new Error("User does not exists");
+      }
+
+      const connection = await ConnectionRequest.findOne({
+        fromUserId: userId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+      console.log("connection", connection);
+      if (!connection) {
+        throw new Error("Connection does not exists");
+      }
+      connection.status = status;
+      await connection.save();
+      res.status(200).status(`Connection request` + status);
     } catch (error) {
       res.status(400).send(error.message);
     }
