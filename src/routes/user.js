e47fe74c6f2d6f9fd7954ce677763255ba117978
Rajeev Kumar <rajeevk@leewayhertz.com>
2025-11-11 10:45:30 +0530

    connection accepted api created

--- src/routes/user.js
+++ src/routes/user.js
@@ -4,13 +4,30 @@ const User = require("../models/user");
 const userAuth = require("../middleware/auth");
 const connectionRequest = require("../models/connectionRequest");
 
-userRouter.get("/user/requests", userAuth, async (req, res) => {
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   try {
     const loggedInUser = req.user;
-    const connectionRequests = await connectionRequest.find({
-      status: "interested",
-      toUserId: loggedInUser._id,
-    }).populate("fromUserId", ["name", "age", "email", "gender", "skills"]);
+    const connectionRequests = await connectionRequest
+      .find({
+        status: "interested",
+        toUserId: loggedInUser._id,
+      })
+      .populate("fromUserId", ["name", "age", "email", "gender", "skills"]);
+    res.status(200).json(connectionRequests);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.get("/user/connection/accepted", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const connectionRequests = await connectionRequest
+      .find({
+        status: "accepted",
+        toUserId: loggedInUser._id,
+      })
+      .populate("fromUserId", ["name", "age", "email", "gender", "skills"]);
     res.status(200).json(connectionRequests);
   } catch (error) {
     res.status(500).json({ error: error.message });
